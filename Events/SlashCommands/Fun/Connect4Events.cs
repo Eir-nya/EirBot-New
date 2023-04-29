caa746d8562f4adf8dc55c17a3dd4a189c8c3d02
Eir-nya <Eir.nyanbinary@gmail.com>
2023-04-28 21:56:46 -0500

    Add Connect 4

---
+++ Events/SlashCommands/Fun/Connect4Events.cs
@@ -0,0 +1,122 @@
+using DisCatSharp;
+using DisCatSharp.ApplicationCommands;
+using DisCatSharp.ApplicationCommands.Attributes;
+using DisCatSharp.ApplicationCommands.Context;
+using DisCatSharp.Enums;
+using DisCatSharp.Entities;
+using DisCatSharp.EventArgs;
+
+namespace EirBot_New.Events.Connect4;
+[SlashCommandGroup("Fun", "Fun and games", true, false), EventHandler]
+public class Connect4Events : ApplicationCommandsModule {
+	private static Dictionary<long, Connect4Game> games = new Dictionary<long, Connect4Game>();
+
+	[SlashCommand("Connect4", "Play Connect Four with another user.", true, false)]
+	public static async Task Connect4(InteractionContext context, [Option("Opponent", "Opponent to play against.\nThey will be yellow, you will be red.", false)] DiscordUser opponent) {
+		// Create game
+		DateTimeOffset timestamp = DateTimeOffset.Now;
+		long id = timestamp.ToUnixTimeSeconds();
+		Connect4Game game = new Connect4Game() { client = context.Client, gameID = id, timestamp = timestamp };
+		game.SetPlayers(context.User, opponent);
+		games[game.gameID] = game;
+
+		await context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+			.AddEmbed(new DiscordEmbedBuilder()
+				.WithColor(DiscordColor.Blurple)
+				.WithAuthor(context.User.Username, null, context.User.AvatarUrl)
+				.WithDescription(String.Format("{0}, {1} has challenged you to a Connect Four match!\nDo you accept?", opponent.Mention, context.User.Username))
+			)
+			.AddComponents(new DiscordComponent[] {
+				new DiscordButtonComponent(ButtonStyle.Success, "Connect4_start_" + id, "You're on!", false, new DiscordComponentEmoji(1087449113202806834)),
+				new DiscordButtonComponent(ButtonStyle.Danger, "Connect4_refuse_" + id, "Pass", false, new DiscordComponentEmoji(865085988162371624))
+				})
+			);
+	}
+
+	[Event(DiscordEvent.ComponentInteractionCreated)]
+	public static async Task AcceptDecline(DiscordClient client, ComponentInteractionCreateEventArgs args) {
+		if (args.Message.Embeds.Count != 1)
+			return;
+
+		long id = 0;
+		string[] splitParts = args.Id.Split("_", StringSplitOptions.None);
+		if (splitParts.Length > 2)
+			id = Convert.ToInt64(splitParts[2]);
+
+		if (games.ContainsKey(id))
+			if (args.Interaction.User != games[id].yellowPlayer)
+				return;
+
+		// Decline invite
+		if (args.Id.StartsWith("Connect4_refuse")) {
+			args.Handled = true;
+			if (!games.ContainsKey(id))
+				await args.Interaction.CreateResponseAsync(InteractionResponseType.UpdateMessage, new DiscordInteractionResponseBuilder()
+					.WithContent("*(Game no longer active.)*")
+					);
+			else
+				await args.Interaction.CreateResponseAsync(InteractionResponseType.UpdateMessage, new DiscordInteractionResponseBuilder()
+					.WithContent("*Game refused.*")
+					);
+		// Accept invite
+		} else if (args.Id.StartsWith("Connect4_start")) {
+			args.Handled = true;
+			if (!games.ContainsKey(id))
+				await args.Interaction.CreateResponseAsync(InteractionResponseType.UpdateMessage, new DiscordInteractionResponseBuilder()
+					.WithContent("*(Game no longer active.)*")
+					);
+			else {
+				Connect4Game game = games[id];
+				game.message = args.Message;
+				game.Init();
+
+				await args.Message.ModifyAsync(game.Display());
+				await args.Message.CreateReactionAsync(DiscordEmoji.FromName(client, ":one:"));
+				await args.Message.CreateReactionAsync(DiscordEmoji.FromName(client, ":two:"));
+				await args.Message.CreateReactionAsync(DiscordEmoji.FromName(client, ":three:"));
+				await args.Message.CreateReactionAsync(DiscordEmoji.FromName(client, ":four:"));
+				await args.Message.CreateReactionAsync(DiscordEmoji.FromName(client, ":five:"));
+				await args.Message.CreateReactionAsync(DiscordEmoji.FromName(client, ":six:"));
+				await args.Message.CreateReactionAsync(DiscordEmoji.FromName(client, ":seven:"));
+			}
+		}
+	}
+
+	[Event(DiscordEvent.MessageReactionAdded)]
+	public static async Task UpdateGame(DiscordClient client, MessageReactionAddEventArgs args) {
+		if (args.User == client.CurrentUser)
+			return;
+		if (args.Message.Embeds.Count < 1)
+			return;
+		if (args.Message.Embeds[0].Timestamp == null)
+			return;
+		if (!games.ContainsKey(args.Message.Embeds[0].Timestamp.Value.ToUnixTimeSeconds()))
+			return;
+
+		Connect4Game game = games[args.Message.Embeds[0].Timestamp.Value.ToUnixTimeSeconds()];
+		if (game.gameOver)
+			return;
+
+		if ((game.nextTurnYellow && args.User == game.yellowPlayer) || args.User == game.redPlayer) {
+			int column = game.ParseColumn(args.Emoji);
+			if (column > -1 && game.ColumnFree(column)) {
+				game.Place(column);
+				await args.Message.ModifyAsync(game.Display());
+				if (game.gameOver)
+					new Thread(async () => await RemoveOwnReactionsAsync(client, args.Message)).Start();
+			}
+		}
+
+		await args.Message.DeleteReactionAsync(args.Emoji, args.User);
+	}
+
+	private static async Task RemoveOwnReactionsAsync(DiscordClient client, DiscordMessage msg) {
+		await msg.DeleteOwnReactionAsync(DiscordEmoji.FromName(client, ":one:"));
+		await msg.DeleteOwnReactionAsync(DiscordEmoji.FromName(client, ":two:"));
+		await msg.DeleteOwnReactionAsync(DiscordEmoji.FromName(client, ":three:"));
+		await msg.DeleteOwnReactionAsync(DiscordEmoji.FromName(client, ":four:"));
+		await msg.DeleteOwnReactionAsync(DiscordEmoji.FromName(client, ":five:"));
+		await msg.DeleteOwnReactionAsync(DiscordEmoji.FromName(client, ":six:"));
+		await msg.DeleteOwnReactionAsync(DiscordEmoji.FromName(client, ":seven:"));
+	}
+}
---
+++ Events/SlashCommands/Fun/Connect4Game.cs
@@ -0,0 +1,182 @@
+using DisCatSharp;
+using DisCatSharp.Entities;
+
+namespace EirBot_New.Events.Connect4;
+public class Connect4Game {
+	private static string EMPTY = ":white_circle:";
+	private static string YELLOW = ":yellow_circle:";
+	private static string RED = ":red_circle:";
+
+	private enum Space {
+		EMPTY,
+		YELLOW,
+		RED
+	}
+
+	public DiscordClient client;
+	public long gameID;
+	public DateTimeOffset timestamp;
+	public DiscordMessage message;
+	private Space[,] board;
+
+	public bool started { get; private set; } = false;
+	public bool gameOver { get; private set; } = false;
+	public bool nextTurnYellow { get; private set; } = true;
+	private bool winnerYellow = false;
+	public DiscordUser redPlayer { get; private set; }
+	public DiscordUser yellowPlayer { get; private set; }
+
+	public void Init() {
+		board = new Space[7, 6];
+		for (int col = 0; col < 7; col++)
+			for (int row = 0; row < 6; row++)
+				board[col, row] = Space.EMPTY;
+	}
+
+	public void SetPlayers(DiscordUser redPlayer, DiscordUser yellowPlayer) {
+		this.redPlayer = redPlayer;
+		this.yellowPlayer = yellowPlayer;
+	}
+
+	private string GetSpaceCharacter(Space space) {
+		switch (space) {
+			case Space.YELLOW:
+				return YELLOW;
+			case Space.RED:
+				return RED;
+			default:
+				return EMPTY;
+		}
+	}
+
+	public DiscordMessageBuilder Display() {
+		DiscordMessageBuilder response = new DiscordMessageBuilder()
+			.AddEmbed(new DiscordEmbedBuilder()
+				.WithColor(nextTurnYellow ? DiscordColor.Yellow : DiscordColor.Red)
+				.WithAuthor(nextTurnYellow ? yellowPlayer.Username : redPlayer.Username, null, nextTurnYellow ? yellowPlayer.AvatarUrl : redPlayer.AvatarUrl)
+				.WithDescription(RED + ": " + redPlayer.Mention + (gameOver && !nextTurnYellow ? " :confetti_ball:" : "") + "\n" + YELLOW + ": " + yellowPlayer.Mention + (gameOver && nextTurnYellow ? " :confetti_ball:" : "") + "\n" + DisplayBoard())
+				.WithFooter(StatusMessage(), client.CurrentUser.AvatarUrl)
+				.WithTimestamp(timestamp)
+			);
+		return response;
+	}
+
+	public int ParseColumn(DiscordEmoji emoji) {
+		switch(emoji.GetDiscordName()) {
+			case ":one:":
+				return 0;
+			case ":two:":
+				return 1;
+			case ":three:":
+				return 2;
+			case ":four:":
+				return 3;
+			case ":five:":
+				return 4;
+			case ":six:":
+				return 5;
+			case ":seven:":
+				return 6;
+
+			default:
+				break;
+		}
+		return -1;
+	}
+
+	private void Eval(int col, int row) {
+		Space color = board[col, row];
+		gameOver = EvalHorizontals(col, row, color);
+		if (!gameOver)
+			gameOver = EvalVerticals(col, row, color);
+		if (!gameOver)
+			gameOver = EvalDiagonals(col, row, color);
+	}
+
+	private bool EvalHorizontals(int col, int row, Space color) {
+		int matchingCount = 0;
+		for (int i = col - 3; i <= col + 3; i++)
+			if (i > -1 && i < 7 && board[i, row] == color) {
+				matchingCount++;
+				if (matchingCount == 4)
+					return true;
+			} else
+				matchingCount = 0;
+		return false;
+	}
+
+	private bool EvalVerticals(int col, int row, Space color) {
+		int matchingCount = 0;
+		for (int i = row - 3; i < row + 3; i++) {
+			if (i > -1 && i < 6 && board[col, i] == color) {
+				matchingCount++;
+				if (matchingCount == 4)
+					return true;
+			} else
+				matchingCount = 0;
+		}
+		return false;
+	}
+
+	private bool EvalDiagonals(int col, int row, Space color) {
+		int matchingCount = 0;
+		for (int i = -3; i < 3; i++)
+			if (col + i > -1 && row + i > -1 && col + i < 7 && row + i < 6 && board[col + i, row + i] == color) {
+				matchingCount++;
+				if (matchingCount == 4)
+					return true;
+			} else
+				matchingCount = 0;
+
+		matchingCount = 0;
+		for (int i = -3; i < 3; i++) {
+			if (col + i > -1 && row - i > -1 && col + i < 7 && row - i < 6 && board[col + i, row - i] == color) {
+				matchingCount++;
+				if (matchingCount == 4)
+					return true;
+			} else
+				matchingCount = 0;
+		}
+		return false;
+	}
+
+	private void UpdateStatus() {
+		if (!gameOver)
+			nextTurnYellow = !nextTurnYellow;
+	}
+
+	private string DisplayBoard() {
+		string boardString = "";
+		for (int row = 5; row >= 0; row--) {
+			for (int col = 0; col < 7; col++)
+				boardString += GetSpaceCharacter(board[col, row]);
+			if (row > 0)
+				boardString += "\n";
+		}
+		return boardString;
+	}
+
+	private string StatusMessage() {
+		if (gameOver)
+			return String.Format("{0} won!", (winnerYellow ? yellowPlayer : redPlayer).Username);
+		else
+			return String.Format("{0}'s turn.", (nextTurnYellow ? yellowPlayer : redPlayer).Username);
+	}
+
+	public bool ColumnFree(int col) {
+		return board[col, 5] == Space.EMPTY;
+	}
+
+	public void Place(int col) {
+		Space color = nextTurnYellow ? Space.YELLOW : Space.RED;
+
+		for (int bottomMost = 0; bottomMost < 6; bottomMost++)
+			if (board[col, bottomMost] == Space.EMPTY) {
+				board[col, bottomMost] = color;
+				Eval(col, bottomMost);
+				break;
+			}
+
+		UpdateStatus();
+	}
+}
